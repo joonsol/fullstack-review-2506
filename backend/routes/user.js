a800bc6b218761d8183138eae3caf82eae4a2f21
joonsol <71648638+joonsol@users.noreply.github.com>
2025-07-05 15:02:07 +0900

    Update user.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -87,7 +87,7 @@ router.post("/login", async (req, res) => {
 
     res.cookie('token', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',
       sameSite: "strict",
       maxAge: 24 * 60 * 60 * 1000
     })
