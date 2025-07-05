0f4798bb564391717d14c18af0bd2c58b7b31b2d
joonsol <71648638+joonsol@users.noreply.github.com>
2025-07-05 15:08:41 +0900

    Update user.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -88,7 +88,7 @@ router.post("/login", async (req, res) => {
     res.cookie('token', token, {
       httpOnly: true,
       sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',
-      sameSite: "strict",
+      secure: process.env.NODE_ENV === 'production',
       maxAge: 24 * 60 * 60 * 1000
     })
 
@@ -157,28 +157,28 @@ router.delete("/delete/:userId", async (req, res) => {
 })
 
 
-router.post("/verify-token",(req,res)=>{
-  const token =req.cookies.token
+router.post("/verify-token", (req, res) => {
+  const token = req.cookies.token
 
-  if(!token){
+  if (!token) {
     return res.status(400).json({
-      isValid:false,
-      message:"토큰이 없습니다."
+      isValid: false,
+      message: "토큰이 없습니다."
     })
   }
   try {
-    const decoded =jwt.verify(token, process.env.JWT_SECRET)
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
     return res.status(200).json({
-      isValid:true,
-      user:decoded
+      isValid: true,
+      user: decoded
     })
   } catch (error) {
     return res.status(401).
-    json({
-      
-      isValid:false,
-      message:"유효하지 않은 토큰입니다."
-    })
+      json({
+
+        isValid: false,
+        message: "유효하지 않은 토큰입니다."
+      })
   }
 })
 
